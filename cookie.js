c0dd352d5607bf15d26ec2a2253bfe84a74248a5
fushihao <fushihao@mifengkong.cn>
2019-04-03 10:37:59 +0800

    static

---
+++ cookie.js
@@ -0,0 +1,59 @@
+var cookie = {
+    set: function (key, val, time) {//设置cookie方法
+        var date = new Date(); //获取当前时间
+        var expiresDays = time;  //将date设置为n天以后的时间
+        date.setTime(date.getTime() + expiresDays * 24 * 3600 * 1000); //格式化为cookie识别的时间
+        document.cookie = key + "=" + val + ";expires=" + date.toGMTString();  //设置cookie
+    },
+    get: function (key) {//获取cookie方法
+        /*获取cookie参数*/
+        var getCookie = document.cookie.replace(/[ ]/g, "");  //获取cookie，并且将获得的cookie格式化，去掉空格字符
+        var arrCookie = getCookie.split(";")  //将获得的cookie以"分号"为标识 将cookie保存到arrCookie的数组中
+        var tips;  //声明变量tips
+        for (var i = 0; i < arrCookie.length; i++) {   //使用for循环查找cookie中的tips变量
+            var arr = arrCookie[i].split("=");   //将单条cookie用"等号"为标识，将单条cookie保存为arr数组
+            if (key === arr[0]) {  //匹配变量名称，其中arr[0]是指的cookie名称，如果该条变量为tips则执行判断语句中的赋值操作
+                tips = arr[1];   //将cookie的值赋给变量tips
+                break;   //终止for循环遍历
+            }
+        }
+        return tips;
+    },
+    delete: function (key) { //删除cookie方法
+        var date = new Date(); //获取当前时间
+        date.setTime(date.getTime() - 10000); //将date设置为过去的时间
+        document.cookie = key + "= ; expires =" + date.toGMTString();//设置cookie
+    }
+};
+
+
+function checkCookie() {
+    let id = cookie.get("player_id");
+    let name = cookie.get("player_name");
+    let token = cookie.get("player_token");
+    if (typeof (id) !== "undefined" && typeof (name) !== "undefined"  && typeof (token) !== "undefined" ) {
+        $("#nav_left").html("<a href='index.html'>"+name+", 欢迎访问</a>");
+        $("#nav_right").html("<span class=\"r_nav\">[ <a rel=\"nofollow\" " +
+            "href=\"player.html\">个人中心</a> ]</span><span class=\"pipe\">|</span>" +
+            "<span class=\"r_nav\">[ <a href=\"javascript:logout();\" rel=\"nofollow\">退出登陆</a> ]</span>")
+    }else {
+        $("#nav_left").html("<a href='index.html'>您好, 欢迎访问</a>");
+        $("#nav_right").html("<span class=\"r_nav\">[ <a rel=\"nofollow\" " +
+            "href=\"login.html\">登陆</a> ]</span>")
+    }
+}
+function logout() {
+    cookie.delete("player_id");
+    cookie.delete("player_name");
+    cookie.delete("player_token");
+    HiAlert("退出成功");
+    window.setTimeout("window.location='index.html'",2000);
+}
+
+function token_timeout() {
+    cookie.delete("player_id");
+    cookie.delete("player_name");
+    cookie.delete("player_token");
+    HiAlert("令牌过期请重新登陆");
+    window.setTimeout("window.location='index.html'",2000);
+}
---
+++ index.js
@@ -0,0 +1,174 @@
+function getContest(obj) {
+    $.ajax(
+        {
+            url: "http://127.0.0.1:8000/v1/index/getContest",
+            method: "GET",
+            success: function (evt, req,) {
+                let html = "";
+                if (req === "success") {
+                    for (let i = 0; i < evt['data'].length; i++) {
+                        html += "<li class=\"item\">\n" +
+                            "        <h4>" + evt['data'][i]['name'];
+                        if (evt['data'][i]['station'].length > 0) html += "&nbsp;&nbsp;&nbsp;&nbsp;(" + evt['data'][i]['station'] + ")";
+                        html += "<b>+</b></h4>\n" +
+                            "        <div class=\"info\" id=\"" + evt['data'][i]['id'] + "\"></div>\n" +
+                            "    </li>\n";
+                    }
+                    $(obj).html(html)
+                } else {
+                    HiAlert("ajax fail");
+                    //todo fail handle
+                }
+            }
+        }
+    );
+}
+
+
+showMatch = function (obj, obj_c, speed, obj_type, Event) {
+    $(obj).off(Event).on(Event, function () {
+        if ($(this).next().is(":visible")) {
+            $(this).next()['isSetEvents'] = true;
+            $(this).next().slideUp(speed).end().removeClass("selected");
+            $(this).find("b").html("+")
+        } else {
+            $(obj_c).slideUp(speed);
+            $(obj).removeClass("selected");
+            $(obj).find("b").html("+");
+            $(this).next().slideDown(speed).end().addClass("selected");
+            if ($(this).next().html() === "") {
+                $.GetMatch($(this).next());
+            }
+            $(this).find("b").html("-")
+        }
+    });
+    $("obj").unbind(Event);
+};
+
+jQuery.GetMatch = function (obj) {
+    let contestId = obj.attr("id");
+    $.ajax({
+        url: "http://127.0.0.1:8000/v1/index/getContestMatch/" + contestId,
+        success: function (evt, req, settings) {
+            if (req === "success") {
+                let html = "<table class=\"table table-border table-bg table-bordered table-striped table-hover\">\n" +
+                    "    <thead>\n" +
+                    "    <tr>\n" +
+                    "        <th>项目</th>\n" +
+                    "        <th>组别</th>\n" +
+                    "        <th>性别</th>\n" +
+                    "        <th>赛别</th>\n" +
+                    "        <th>人数</th>\n" +
+                    "        <th>组数</th>\n" +
+                    "        <th>录取</th>\n" +
+                    "        <th>备注</th>\n" +
+                    "    </tr>\n" +
+                    "    </thead>\n" +
+                    "    <tbody>\n";
+                for (let i = 0; i < evt['data'].length; i++) {
+                    html += "<tr onclick='MatchOnClick(" + evt['data'][i]['group_num'] + "," + evt['data'][i]['id'] + ")'>\n" +
+                        "        <td>" + evt['data'][i]['match_name'] + "</td>\n" +
+                        "        <td>" + evt['data'][i]['group_type'] + "</td>\n" +
+                        "        <td>" + evt['data'][i]['gender'] + "</td>\n" +
+                        "        <td>" + evt['data'][i]['match_type'] + "</td>\n" +
+                        "        <td>" + evt['data'][i]['player_num'] + "</td>\n" +
+                        "        <td>" + evt['data'][i]['group_num'] + "</td>\n" +
+                        "        <td>" + evt['data'][i]['enter'] + "</td>\n" +
+                        "        <td>" + evt['data'][i]['remark'] + "</td>\n" +
+                        "    </tr>\n";
+                }
+                if (evt['data'].length === 0) {
+                    html += "        <tr><td colspan='8' style='text-align: center'>暂无数据</td></tr>\n"
+                }
+                html += "</tbody>\n" +
+                    "</table>";
+                obj.html(html);
+            } else {
+                HiAlert("ajax fail")
+            }
+
+        },
+        //dataType: dataType
+    });
+};
+
+
+getContest("#Huifold1");
+
+$("body").on("click", "#Huifold1", function () {
+    showMatch("#Huifold1 .item h4", "#Huifold1 .item .info", "fast", 1, "click"); /*5个参数顺序不可打乱，分别是：相应区,隐藏显示的内容,速度,类型,事件*/
+});
+MatchOnClick = function (num, id) {
+    modaldemo(num, id)
+};
+
+function modaldemo(num, id) {
+    let html = "";
+    for (let i = 1; i <= num; i++) {
+        html += "<button  onclick='OnGroupBtnClick(" + id + "," + i + ")' class=\"btn btn-primary-outline radius mb-5 mr-5\" >第" + i + "组</button>";
+    }
+    $("div.modal-body").html(html);
+    $("#modal-demo").modal("show")
+}
+
+function OnGroupBtnClick(id, group) {
+    $.ajax({
+        url: "http://127.0.0.1:8000/v1/index/getMatchScore/" + id + "/" + group,
+        method: "GET",
+        success: function (evt, req) {
+            if (req === "success") {
+                let html = "<table class=\"table table-border table-bg table-bordered table-striped table-hover\">\n" +
+                    "    <thead>\n" +
+                    "    <tr>\n" +
+                    "        <th>名次</th>\n" +
+                    "        <th>道次</th>\n" +
+                    "        <th>头盔号</th>\n" +
+                    "        <th>姓名</th>\n" +
+                    "        <th>单位</th>\n" +
+                    "        <th>成绩</th>\n" +
+                    "        <th>备注</th>\n" +
+                    "    </tr>\n" +
+                    "    </thead>\n" +
+                    "    <tbody>\n";
+                for (let i = 0; i < evt['data'].length; i++) {
+                    html += "<tr>\n" +
+                        "        <td>" + evt['data'][i]['no'] + "</td>\n" +
+                        "        <td>" + evt['data'][i]['row_num'] + "</td>\n" +
+                        "        <td>" + evt['data'][i]['head_num'] + "</td>\n" +
+                        "        <td>" + evt['data'][i]['name'] + "</td>\n" +
+                        "        <td>" + evt['data'][i]['organize'] + "</td>\n" +
+                        "        <td>" + evt['data'][i]['time_score'] + "</td>\n" +
+                        "        <td>" + evt['data'][i]['remark'] + "</td>\n" +
+                        "    </tr>\n";
+                }
+                if (evt['data'].length === 0) {
+                    html += "        <tr><td colspan='8' style='text-align: center'>暂无数据</td></tr>\n"
+                }
+                html += "</tbody>\n" +
+                    "</table>";
+                $("div.modal-body").html(html);
+            }
+        }
+    })
+
+}
+
+HiAlert = function (string, time = 2000) {
+    $.Huimodalalert(string, time)
+};
+
+
+$(function () {
+    jQuery("#slider-1 .slider").slide({
+        mainCell: ".bd ul",
+        titCell: ".hd li",
+        trigger: "mousemove",
+        effect: "leftLoop",
+        autoPlay: true,
+        delayTime: 850,
+        interTime: 1500,
+        pnLoop: true,
+        titOnClassName: "active"
+    })
+});
+checkCookie();
